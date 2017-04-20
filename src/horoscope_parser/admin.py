7539610e916b93d2ab8965c897bcefcefd8a819d
adiletmaratov <adilet.maratov@gmail.com>
2017-04-20 18:55:54 +0600

    add model for daily common horoscopes

--- src/horoscope_parser/admin.py
+++ src/horoscope_parser/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import DailyCommonHoroscope
 
-# Register your models here.
+
+admin.site.register(DailyCommonHoroscope)
+
--- src/horoscope_parser/models.py
+++ src/horoscope_parser/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from .constants import *
 
-# Create your models here.
+
+ANIMALS_CHOICES = (
+    (1, ARIES),
+    (2, TAURUS),
+    (3, GEMINI),
+    (4, CANCER),
+    (5, LEO),
+    (6, VIRGO),
+    (7, LIBRA),
+    (8, SCORPIO),
+    (9, SAGITTARIUS),
+    (10, CAPRICORN),
+    (11, AQUARIOU),
+    (12, PISCES),
+)
+
+
+class DailyCommonHoroscope(models.Model):
+    name = models.CharField(max_length=254, choices=ANIMALS_CHOICES,
+                            db_index=True)
+    description = models.TextField()
+    date = models.DateField(db_index=True)
+
+    class Meta:
+        verbose_name = "Ежедневный гороскоп"
+        verbose_name_plural = "Ежедневные гороскопы"
